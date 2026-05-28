8bb4d68852c95a74049e6408fdcb0f48d0b87607
Gurjeet Singh Bedi <Gurjeet.Bedi@gds.ey.com>
2026-05-28 17:21:51 +0530

    new code

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 import unicorn
 
 app = FastAPI()
+
 @app.get("/")
 def hello():
     return {"message": "Hello World!"}
 
-#sample
-
+# API to display name entered by user
+@app.get("/name")
+def displayName(name: str):
+    return {"message": f"Hello {name}"}
